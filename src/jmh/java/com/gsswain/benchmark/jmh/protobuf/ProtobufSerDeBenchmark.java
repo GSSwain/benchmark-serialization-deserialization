c9f8a1f036fdebef6276f52e25684cee3a518ab1
GSSwain <contact@gsswain.com>
2025-11-29 20:36:22 +1100

    Print environment as part of running benchmarks

--- src/jmh/java/com/gsswain/benchmark/jmh/protobuf/ProtobufSerDeBenchmark.java
+++ src/jmh/java/com/gsswain/benchmark/jmh/protobuf/ProtobufSerDeBenchmark.java
@@ -3,15 +3,18 @@ package com.gsswain.benchmark.jmh.protobuf;
 import com.gsswain.benchmark.serialization.model.proto.SocialMediaPost;
 import com.gsswain.benchmark.serialization.protobuf.ProtobufMapper;
 import com.gsswain.benchmark.util.DataGenerator;
+import com.gsswain.benchmark.util.EnvironmentUtil;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
@@ -25,6 +28,11 @@ import org.openjdk.jmh.infra.Blackhole;
 public class ProtobufSerDeBenchmark {
 
     private static final ProtobufMapper protobufMapper = new ProtobufMapper();
+
+    @Setup(Level.Trial)
+    public void setup() {
+        EnvironmentUtil.printEnvironment();
+    }
 
     @State(Scope.Thread)
     public static class BenchmarkState {
