1895fbd11442933d9cd7c023a4e4d974c7784f25
GSSwain <contact@gsswain.com>
2025-12-13 23:16:51 +1100

    Moves all SerDe Benchmarks to JavaSerDeBenchmark for easier visualization of report

--- src/jmh/java/com/gsswain/benchmark/jmh/json/JsonSerDeBenchmark.java
+++ src/jmh/java/com/gsswain/benchmark/jmh/json/JsonSerDeBenchmark.java
@@ -3,41 +3,16 @@ package com.gsswain.benchmark.jmh.json;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gsswain.benchmark.serialization.model.pojo.SocialMediaPost;
 import com.gsswain.benchmark.util.DataGenerator;
-import com.gsswain.benchmark.util.EnvironmentUtil;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 
-@BenchmarkMode({Mode.AverageTime, Mode.Throughput})
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
-@State(Scope.Thread)
-@Warmup(iterations = 5, time = 1)
-@Measurement(iterations = 10, time = 1)
-@Fork(2)
 public class JsonSerDeBenchmark {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Setup(Level.Trial)
-    public void setup() {
-        EnvironmentUtil.printEnvironment();
-    }
-
-    @State(Scope.Thread)
     public static class BenchmarkState {
-        SocialMediaPost socialMediaPost = DataGenerator.generateSocialMediaPost();
-        byte[] serializedSocialMediaPost;
+        public SocialMediaPost socialMediaPost = DataGenerator.generateSocialMediaPost();
+        public byte[] serializedSocialMediaPost;
 
         public BenchmarkState() {
             try {
@@ -48,12 +23,10 @@ public class JsonSerDeBenchmark {
         }
     }
 
-    @Benchmark
     public void jsonSerialization(BenchmarkState state, Blackhole blackhole) throws IOException {
         blackhole.consume(objectMapper.writeValueAsBytes(state.socialMediaPost));
     }
 
-    @Benchmark
     public void jsonDeserialization(BenchmarkState state, Blackhole blackhole) throws IOException {
         blackhole.consume(objectMapper.readValue(state.serializedSocialMediaPost, SocialMediaPost.class));
     }
